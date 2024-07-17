7b5b89a5eb93cfb8af4d4ad8cf9190da285753df
maxamadBashi <maxamedbaashi132@gmail.com>
2024-07-17 14:12:53 -0700

    comit

--- src/components/DataList.jsx
+++ src/components/DataList.jsx
@@ -1,11 +1,13 @@
-import DataItem from "./DataItem";
+// import DataItem from "./DataItem";
 
-export default function DataList({ items }) {
-  return (
-    <ul className="w-full flex flex-col justify-center items-center">
-      {items.map((item, index) => (
-        <DataItem key={index} {...item} />
-      ))}
-    </ul>
-  );
-}
+// export default function DataList({ items }) {
+//   return (
+//     <ul className="w-full flex flex-col justify-center items-center">
+//       {items.map((item, index) => (
+//         <DataItem key={index} {...item} />
+//       ))}
+//     </ul>
+//   );
+// }
+
+
