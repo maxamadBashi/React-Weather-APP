3f2bd8f8d5094718e5bdbc4f985f76fa5e39b13a
maxamadBashi <maxamedbaashi132@gmail.com>
2024-07-23 06:47:24 -0700

    search update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -21,7 +21,7 @@ export default function Search({ onSearch }) {
       <i className="fa-solid fa-magnifying-glass opacity-75"></i>
       <input
         className="w-full outline-0 bg-transparent placeholder:text-white placeholder:opacity-75"
-        placeholder="Lviv, UA"
+        placeholder="New York, USA"
         name="search"
         type="text"
         value={value}
