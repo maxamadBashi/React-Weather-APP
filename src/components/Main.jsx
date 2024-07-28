a177c702a34180d2fbd3121901e96a083bfc8993
maxamadBashi <165473340+maxamadBashi@users.noreply.github.com>
2024-07-28 13:14:29 -0700

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,7 @@ import WeatherSection from "./WeatherSection";
 import ForecastList from "./ForecastList";
 
 export default function Main({ city }) {
+  // If no city is provided, render a message prompting the user to enter a city
   if (!city) {
     return (
       <main className="max-w-screen-md p-4 flex flex-1 flex-col justify-center items-center">
@@ -14,25 +15,28 @@ export default function Main({ city }) {
     );
   }
 
+  // Use custom hooks to fetch weather and forecast data
   const { weatherData, weatherLoading, weatherError } = useWeather({ city });
-  const { forecastData, forecastLoading, forecastError } = useForecast({
-    city,
-  });
+  const { forecastData, forecastLoading, forecastError } = useForecast({ city });
 
   return (
     <main className="max-w-screen-md p-4 flex flex-1 flex-col justify-center items-center gap-4">
+      {/* Show a loading spinner while both weather and forecast data are being fetched */}
       {weatherLoading && forecastLoading && (
         <img className="w-32" src="loading.svg" alt="Loading"></img>
       )}
+      {/* Show an error message if both weather and forecast data fail to load */}
       {weatherError && forecastError && (
         <section className="w-64 h-32 p-4 flex flex-col justify-center items-center border border-error border-opacity-75 rounded-xl bg-error bg-opacity-75 shadow-[0_0_16px_0_rgba(255,0,0,0.75)] backdrop-blur">
           <span className="text-xl">Error</span>
           <span className="opacity-75">City not found</span>
         </section>
       )}
+      {/* Render WeatherSection if weather data is successfully fetched */}
       {!weatherLoading && !weatherError && (
         <WeatherSection weatherData={weatherData} />
       )}
+      {/* Render ForecastList if forecast data is successfully fetched */}
       {!forecastLoading && !forecastError && (
         <ForecastList forecastData={forecastData} />
       )}
